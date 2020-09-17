51df32621dc8274dda2c2aa0b4c22eecf0bdf1d5
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-09-17 22:52:03 +0100

    first commit

---
+++ Assets/Scripts/DoorsColl.cs
@@ -0,0 +1,28 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mirror.Examples.Pong
+{
+    public class DoorsColl : NetworkBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Finish")
+            {
+                collision.gameObject.transform.position = new Vector3(0, -5, 10);
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mirror.Examples.Pong
+{
+    public class Player : NetworkBehaviour
+    {
+        public float UpSpeed = 50.0f;
+        public float LateralSpeed = 50.0f;
+        //public GameObject[] Doors;
+
+        Rigidbody2D rb;
+        Vector3 pos;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            pos = transform.position;
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            // Movimento em x
+            if (isLocalPlayer)
+            {
+                float hAxis = Input.GetAxis("Horizontal");
+
+                Vector2 currentVelocity = rb.velocity;
+
+                currentVelocity = new Vector2(hAxis * LateralSpeed, UpSpeed) * Time.fixedDeltaTime;
+
+                rb.velocity = currentVelocity;
+            }
+            
+        }
+
+        
+
+    }
+}
+
